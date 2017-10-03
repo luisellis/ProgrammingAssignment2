e5434738db66e1d71a5449899bf978a27be09946
luisellis <luis.vasquezellis@gmail.com>
2017-10-02 23:46:58 -0400

    Modification of the function
    Finishing the function that was asked by coursera instructors

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2 - Coursera
+## The idea is to create a function that takes advantage of lexical
+## Scoping, Create matrix and find its inverse
 
-## Write a short comment describing this function
+## First part of the function verifies if the value of the inverse is known
+## So that saves time on the computing. setting and getting those values
 
 makeCacheMatrix <- function(x = matrix()) {
-
+i <- NULL
+set<- function(y){
+        x <<- y
+        i <<- NULL
+}
+get <- function() x
+setinv <- function(solve) i <<- solve
+getinv <- function() i
+list(set = set, get = get, setinv = setinv,
+     getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function is the one that takes care of the calculation part in case 
+## The value didn't exist before. If it did exist, then it'll only desplay it
+##using the return function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+i <- x$getinv()
+if(!is.null(i)) {
+        message("getting cached data")
+return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(m)
+        m
+                }
